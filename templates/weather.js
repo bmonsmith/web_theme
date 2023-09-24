7ea183857b426acc68e414f0bcf43763ed380cc3
Brandon Moncrief-Smith <37261239+bmonsmith@users.noreply.github.com>
2023-09-24 12:37:25 -0400

    Templates
    Templates add to the site

---
+++ templates/weather.js
@@ -0,0 +1,7 @@
+document.write('<div style="background-color: #ffffff; color: #000000; width: 175px; padding: 10px; border: 1px #000000 solid; text-align: center; line-height: 1.6em; font-size: 11px; font-family: Verdana;">');
+document.write('<strong><a target="_parent" style="color:#000000;" href="http://www.showmyweather.com/us/michigan/dearborn/">Dearborn, MI Weather</a></strong><br />');
+document.write('<center><table style="color: #000000; border: none;" cellpadding="4"><tr>');
+document.write('<td><center><img class="weatherImg" src="http://www.showmyweather.com/images/fcicons/25x25/sunnyn.gif" /></center>');
+document.write('</td><td><center>68&deg;F, Clear</center></td></tr></table></center>');
+document.write('<strong>Mon:</strong> <img src="http://www.showmyweather.com/images/fcicons/25x25/chancetstorm.gif"> <span style="color: red;">84&deg;F</span> / <span style="color: blue;">68&deg;F</span><br /><strong>Tue:</strong> <img src="http://www.showmyweather.com/images/fcicons/25x25/sunny.gif"> <span style="color: red;">82&deg;F</span> / <span style="color: blue;">62&deg;F</span><br /><strong>Wed:</strong> <img src="http://www.showmyweather.com/images/fcicons/25x25/pcloudy.gif"> <span style="color: red;">81&deg;F</span> / <span style="color: blue;">65&deg;F</span><br />');
+document.write('</div>');
